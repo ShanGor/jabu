2025769fc7be98b93969b71ce1dcef4e11a30a73
Samuel Chan <emschan@live.cn>
2023-09-05 08:51:26 +0800

    make the code more maintainable

--- jabu-core/src/main/java/cn/gzten/jabu/util/TypeUtils.java
+++ jabu-core/src/main/java/cn/gzten/jabu/util/TypeUtils.java
@@ -46,67 +46,78 @@ public class TypeUtils {
                 return o.toString();
             }
         } else if (o instanceof String) {
-            if (target.equals(String.class)) {
-                return o;
-            }
-
-            if (o == null || StringUtil.isBlank((String) o)) return null;
-
-            if (TypeUtils.isLong(target))
-                return Long.parseLong((String) o);
-            if (TypeUtils.isInt(target))
-                return Integer.parseInt((String) o);
-            if (TypeUtils.isShort(target))
-                return Short.parseShort((String) o);
-            if (TypeUtils.isByte(target))
-                return Byte.parseByte((String) o);
-            if (isBoolean(target)) {
-                return Boolean.parseBoolean((String) o);
-            }
-            if (TypeUtils.isDouble(target)){
-                return Double.parseDouble((String) o);
-            }
-            if (TypeUtils.isFloat(target)){
-                return Double.parseDouble((String) o);
-            }
-            if (target.equals(BigDecimal.class)) {
-                return new BigDecimal((String)o);
-            }
-
-            if (target.equals(BigInteger.class)) {
-                return new BigInteger((String)o);
-            }
+            return convertBasicTypesFromString((String) o, target);
         } else if (o instanceof Number) {
-            if (target.equals(String.class)) {
-                return o.toString();
-            }
-            if (isLong(target)) {
-                return ((Number) o).longValue();
-            }
-            if (isInt(target)) {
-                return ((Number) o).intValue();
-            }
-            if (isShort(target)) {
-                return ((Number) o).shortValue();
-            }
-            if (isByte(target)) {
-                return ((Number) o).byteValue();
-            }
-            if (isDouble(target)) {
-                return ((Number) o).doubleValue();
-            }
-            if (isFloat(target)) {
-                return ((Number) o).floatValue();
-            }
-            if (target.equals(BigDecimal.class)) {
-                return new BigDecimal(((Number) o).doubleValue());
-            }
-            if (target.equals(BigInteger.class)) {
-                return BigInteger.valueOf(((Number) o).longValue());
-            }
+            return convertBasicTypesFromNumber((Number) o, target);
         }
-
 
         return o;
     }
+
+    public static Object convertBasicTypesFromString(String o, Class target) {
+        if (target.equals(String.class)) {
+            return o;
+        }
+
+        if (o == null || StringUtil.isBlank(o)) return null;
+
+        if (TypeUtils.isLong(target))
+            return Long.parseLong(o);
+        if (TypeUtils.isInt(target))
+            return Integer.parseInt(o);
+        if (TypeUtils.isShort(target))
+            return Short.parseShort(o);
+        if (TypeUtils.isByte(target))
+            return Byte.parseByte(o);
+        if (isBoolean(target)) {
+            return Boolean.parseBoolean(o);
+        }
+        if (TypeUtils.isDouble(target)){
+            return Double.parseDouble(o);
+        }
+        if (TypeUtils.isFloat(target)){
+            return Double.parseDouble(o);
+        }
+        if (target.equals(BigDecimal.class)) {
+            return new BigDecimal(o);
+        }
+
+        if (target.equals(BigInteger.class)) {
+            return new BigInteger(o);
+        }
+
+        return o;
+    }
+    public static Object convertBasicTypesFromNumber(Number o, Class target) {
+        if (target.equals(String.class)) {
+            return o.toString();
+        }
+        if (isLong(target)) {
+            return o.longValue();
+        }
+        if (isInt(target)) {
+            return o.intValue();
+        }
+        if (isShort(target)) {
+            return o.shortValue();
+        }
+        if (isByte(target)) {
+            return o.byteValue();
+        }
+        if (isDouble(target)) {
+            return o.doubleValue();
+        }
+        if (isFloat(target)) {
+            return o.floatValue();
+        }
+        if (target.equals(BigDecimal.class)) {
+            return new BigDecimal(o.doubleValue());
+        }
+        if (target.equals(BigInteger.class)) {
+            return BigInteger.valueOf(o.longValue());
+        }
+
+        return o;
+    }
+
 }
